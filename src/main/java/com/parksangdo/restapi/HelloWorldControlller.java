4b62077d84592208420bc045d4c9babd38c5abae
snagdo.park.mac <parksangdonews@gmail.com>
2020-08-08 00:54:09 +0900

    Initial commit

---
+++ src/main/java/com/parksangdo/restapi/HelloWorldControlller.java
@@ -0,0 +1,25 @@
+package com.parksangdo.restapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldControlller {
+
+    // get "/hello-world"
+    @GetMapping(path = "hello-world")
+    public String helloWorld() {
+        return "helo World.";
+    }
+
+    // java bean return
+    @GetMapping(path = "hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean();
+    }
+
+
+
+
+
+}
