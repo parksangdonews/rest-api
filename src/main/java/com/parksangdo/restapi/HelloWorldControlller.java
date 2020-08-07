c6f9fbb4157b12497b6b2669d160035ee8e4fc60
parks <parksangdonews@gmail.com>
2020-08-08 02:06:13 +0900

    bean to return json with path variable.

--- src/main/java/com/parksangdo/restapi/HelloWorldControlller.java
+++ src/main/java/com/parksangdo/restapi/HelloWorldControlller.java
@@ -1,6 +1,7 @@
 package com.parksangdo.restapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,12 @@ public class HelloWorldControlller {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello, World!!");
+    }
+
+    // java bean return // auto json return, in body  , with path variable
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariableBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello, World!!, %s", name));
     }
 
 }
