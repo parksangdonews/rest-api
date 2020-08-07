b292ceed11fb254f37acf4fe2cfced48e56753d3
parks <parksangdonews@gmail.com>
2020-08-08 03:56:28 +0900

    feat. create user.

--- src/main/java/com/parksangdo/restapi/user/UserController.java
+++ src/main/java/com/parksangdo/restapi/user/UserController.java
@@ -1,8 +1,6 @@
 package com.parksangdo.restapi.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,12 @@ public class UserController {
     @GetMapping(path = "/users/{id}")
     public User retriveUser(@PathVariable int id) {
         return userDaoService.findOne(id);
+    }
+
+    @PostMapping(path = "/users")
+    public void createUser(@RequestBody User user) {
+        // user -> save
+        User savedUser = userDaoService.save(user);
     }
 
 }
