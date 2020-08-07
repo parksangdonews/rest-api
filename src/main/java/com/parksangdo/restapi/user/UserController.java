960e4e8e8eafe86020c6106c43613487b5950839
parks <parksangdonews@gmail.com>
2020-08-08 04:41:17 +0900

    feat . http status response by status.

--- src/main/java/com/parksangdo/restapi/user/UserController.java
+++ src/main/java/com/parksangdo/restapi/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
@@ -22,8 +23,12 @@ public class UserController {
     }
 
     @GetMapping(path = "/users/{id}")
-    public User retriveUser(@PathVariable int id) {
-        return userDaoService.findOne(id);
+    public User retriveUser(@PathVariable int id) throws UserNotFoundException {
+        User user = userDaoService.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] is not found.", String.valueOf(id)));
+        }
+        return user;
     }
 
     @PostMapping(path = "/users")
@@ -33,5 +38,10 @@ public class UserController {
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build();
     }
+
+
+
+
+
 
 }
