450d723e2c4271a41acc74be904fd789eebcc1d5
parks <parksangdonews@gmail.com>
2020-08-08 04:56:51 +0900

    UserNotFoundException added.

--- src/main/java/com/parksangdo/restapi/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/parksangdo/restapi/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.parksangdo.restapi.exception;
 
+import com.parksangdo.restapi.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,9 +15,18 @@ import java.util.Date;
 @RestController
 @ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+
 }
