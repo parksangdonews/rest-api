fc70757b8f8322eaed613c2c0d41cdcef68b8367
parks <parksangdonews@gmail.com>
2020-08-09 17:27:07 +0900

    jpa & h2 db ready..

--- src/main/java/com/parksangdo/restapi/configuration/SecurityConfiguration.java
+++ src/main/java/com/parksangdo/restapi/configuration/SecurityConfiguration.java
@@ -3,10 +3,20 @@ package com.parksangdo.restapi.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
